8b8cc088180891464ff3b446f6625a3f679ce323
Ancellist <83424461+Ancellist@users.noreply.github.com>
2023-03-15 23:01:39 +0400

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.*;
+
+public class Test6 {
+    public static void main(String args[]){
+        String str = "qweeukfgeeeet";
+        char[] chars = new char[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = str.charAt(i);
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char i : chars ){
+            if(map.containsKey(i)){
+                map.put(i,map.get(i) + 1  );
+            }
+            else {
+                map.put(i,1);
+            }
+        }
+
+        map.entrySet().stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByKey())
+                .forEach(System.out::println);
+
+    }
+}
